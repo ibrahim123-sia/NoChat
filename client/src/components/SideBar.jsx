6e6d7bd01133413ff87e7bbe56808f3e120426f1
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-10-13 18:00:34 +0500

    .

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 const SideBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { chats, setCurrentChat, theme, setTheme, user } = useAppContext();
+  const [search, setSearch] = useState("");
 
-export default SideBar
+  return (
+    <div
+      className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30
+    border-r border-[#80609f]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1"
+    >
+      {/* logo */}
+      <img
+        src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+        alt=""
+        className="w-full max-w-48"
+      />
+
+      {/* new chat */}
+      <button
+        className="flex justify-center items-center w-full py-2 mt-10 text-white 
+      bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer"
+      >
+        <span className="mr-2 text-xl">+</span> New Chat
+      </button>
+    </div>
+  );
+};
+
+export default SideBar;
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,59 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useEffectEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets";
+import { useState } from "react";
 
 const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [chat, setChat] = useState([]);
+  const [selectedChats, setSelectedChats] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme" || "light"));
+
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+  };
+
+  const fetchUserChats = async () => {
+    setChat(dummyChats);
+    setSelectedChats(dummyChats[0]);
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+    else{
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  useEffect(() => {
+    if (user) {
+      fetchUserChats();
+    } else {
+      setChat([]);
+      setSelectedChats(null);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    chat,
+    setChat,
+    selectedChats,
+    setSelectedChats,
+    theme,
+    fetchUser,
+  };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
