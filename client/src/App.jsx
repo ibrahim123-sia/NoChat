c5396beb294a3311913d918465634d4ebd36d560
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-10-13 09:39:38 +0500

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import ChatBox from "./components/ChatBot"
+import Credits from "./pages/Credits"
+import Community from "./pages/Community";
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />}></Route>
+            <Route path="/credits" element={<Credits />}></Route>
+            <Route path="/community" element={<Community />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/ChatBot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatBot = () => {
+  return (
+    <div>
+      hellow
+    </div>
+  )
+}
+
+export default ChatBot
