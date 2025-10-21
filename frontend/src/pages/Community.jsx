2c283342709d355ef35c865ae114299a623c4cb0
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-10-21 22:31:18 +0500

    community page begin

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import dummyPublishedImages from '../assets/assets'
+import Loading from './Loading'
 const Community = () => {
+
+  const [images,setImages]=useState([])
+  const [loading,setLoading]=useState(true)
+
+  const fetchImages=async ()=> {
+    setImages(dummyPublishedImages)
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchImages()
+  },[])
+
+  if(loading) return <Loading/>
+
   return (
-    <div>
+    <div className='p-6 pt-12 xl:px-12 2xl:px-20 w-full mx-auto h-full overflow-y-scroll'>
+      <h2 className='text-xl font-semibold mb-6 text-gray-800 dark:text-purple-100'>Community Images</h2>
+    {images.length >0 ? (
+      <div></div>
+    )
+    :
+    (
+      <p className='text-center text-gray-600 dark:text-purple-200 mt-10'>No Community Image Available</p>
+    )
+    }
       
     </div>
   )
