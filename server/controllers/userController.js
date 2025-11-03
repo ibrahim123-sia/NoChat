f1cd26667e32c8b86e85044d24227225ec125210
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2025-11-03 11:26:46 +0500

    Project Completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,7 +8,7 @@ import rateLimit from "express-rate-limit";
 
 const otpLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 OTP requests per windowMs
+  max: 5, 
   message: "Too many OTP requests from this IP, please try again later",
   skipSuccessfulRequests: true,
 });
